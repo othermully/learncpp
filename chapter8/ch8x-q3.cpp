0010604d7654af85b06f05e6a62fb23fa439f312
othermully <ryancbmullins6@gmail.com>
2026-01-22 17:23:33 -0400

    quiz 3

--- chapter8/ch8x-q3.cpp
+++ chapter8/ch8x-q3.cpp
@@ -1,7 +1,8 @@
 #include <random>
 #include <iostream>
 
-bool check_num(int, int);
+
+bool play_game(int);
 
 namespace rng {
 	// Seed PRNG using std::random_device
@@ -15,12 +16,40 @@ namespace rng {
 	}
 }
 
-void start_game(){
-	int rd { rng::get(1, 7) };
+void play_again(){
+	std::cout << "Would you like to play again (y/n)? ";
+	char c{};
+	std::cin >> c;
+
+}
+
+bool play_game(int rd){
 	std::cout << "Let's play a game. Pick a number between 1-7, you have 7 tries" << '\n';
+	for (int n_guess{ 1 }; n_guess <= 7; ++n_guess){
+		std::cout << "Guess #" << n_guess << ':';
+		int guess{};
+		std::cin >> guess;
+
+		if (guess > rd){
+			std::cout << "You guess is too high." << '\n';
+		}
+		if (guess < rd){
+			std::cout << "Your guess is too low." << '\n';
+		}
+		if (guess == rd){
+			std::cout << "Correct! You win!" << '\n';
+			return true;
+		}
+	}
+	return true;
 }
 
 int main(){
+	int rd { rng::get(1, 7) };
+
+	if (play_game(rd)){
+		play_again();
+	}
 	// Start game
 	// Check user input
 	// If guesses > 7, you lose, ask to play again
